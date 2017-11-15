71484107238c7076f73af391b114d0d6aca0b25d
Dan <dnreimer22@gmail.com>
2017-11-15 15:10:27 +0000

    Added in search functionality with filtering by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
-      @products = Product.all
+      @products = Product.order(:updated_at).all
+      @categories = get_categories
   end
 
   def product
@@ -8,15 +9,28 @@ class ProductsController < ApplicationController
   end
 
   def new_products
-    @new_products = Product.where("created_at >= ?", 2.days.ago.utc)
+    @new_products = Product.where("created_at >= ?", 2.days.ago.utc).order(:created_at)
+    @categories = get_categories
   end
 
   def updated_products
-    @updated_products = Product.where("updated_at >= ?", 2.days.ago.utc)
+    @updated_products = Product.where("updated_at >= ?", 2.days.ago.utc).order(:updated_at)
+    @categories = get_categories
   end
 
   def search_results
     wildcard_keywords = '%' + params[:search_keywords] + '%'
-    @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords)
+
+    if params[:category].to_i == 0
+      @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords)
+    else
+      @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords).where('category_id = ?', params[:category].to_i)
+    end
+
+    @categories = get_categories
+  end
+
+  def get_categories
+      @categories = Category.all
   end
 end
