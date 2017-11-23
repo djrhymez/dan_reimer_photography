6c16b9787637abddae093437b23328a2f5fe7be4
Dan <dnreimer22@gmail.com>
2017-11-23 16:36:09 +0000

    Added kaminari pagination gem. Generated kaminari config file. Paginated index, new and updated products, filter by category and search results.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-      @products = Product.order(:updated_at).all
+      @products = Product.order(:updated_at).page(params[:page]).per(10)
   end
 
   def product
@@ -8,25 +8,25 @@ class ProductsController < ApplicationController
   end
 
   def new_products
-    @new_products = Product.where("created_at >= ?", 2.days.ago.utc).order(:created_at)
+    @new_products = Product.where("created_at >= ?", 2.days.ago.utc).order(:created_at).page(params[:page]).per(10)
   end
 
   def updated_products
-    @updated_products = Product.where("updated_at >= ?", 2.days.ago.utc).order(:updated_at)
+    @updated_products = Product.where("updated_at >= ?", 2.days.ago.utc).order(:updated_at).page(params[:page]).per(10)
   end
 
   def search_results
     wildcard_keywords = '%' + params[:search_keywords] + '%'
 
     if params[:category].to_i == 0
-      @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords)
+      @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords).page(params[:page]).per(10)
     else
-      @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords).where('category_id = ?', params[:category].to_i)
+      @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords).where('category_id = ?', params[:category].to_i).page(params[:page]).per(10)
     end
   end
 
   def products_by_category
     @category = Category.find(params[:category]).name
-    @products = Category.find(params[:category]).products
+    @products = Category.find(params[:category]).products.page(params[:page]).per(10)
   end
 end
