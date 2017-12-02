381e18992019819fb8e133d7faae6a408e3e9d0f
Dan <dnreimer22@gmail.com>
2017-12-02 21:43:15 +0000

    modified a migration file.

--- db/migrate/20171128180241_remove_name_from_clients.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNameFromClients < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :clients, :name, :string
-    remove_column :clients, :password, :string
-    remove_column :clients, :email, :string
-    add_column :clients, :address, :string
-    add_column :clients, :user_id, :integer
-  end
-end
